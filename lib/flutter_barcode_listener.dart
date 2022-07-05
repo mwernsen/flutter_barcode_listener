b0b0103f95d5088acc79d2dae405cc06e2f4b65f
mwernsen <mwernsen@gymstory.nl>
2022-07-05 18:28:00 +0200

    Simplify logic.

--- lib/flutter_barcode_listener.dart
+++ lib/flutter_barcode_listener.dart
@@ -106,35 +106,24 @@ class _BarcodeKeyboardListenerState extends State<BarcodeKeyboardListener> {
   }
 
   void _keyBoardCallback(RawKeyEvent keyEvent) {
-    if (keyEvent.logicalKey.keyId > 255 &&
-        keyEvent.data.logicalKey != LogicalKeyboardKey.enter) return;
-    if ((!_useKeyDownEvent && keyEvent is RawKeyUpEvent) ||
-        (_useKeyDownEvent && keyEvent is RawKeyDownEvent)) {
-      if (keyEvent.data is RawKeyEventDataAndroid) {
-        _controller.sink.add(String.fromCharCode(
-            ((keyEvent.data) as RawKeyEventDataAndroid).codePoint));
-      } else if (keyEvent.data is RawKeyEventDataFuchsia) {
-        _controller.sink.add(String.fromCharCode(
-            ((keyEvent.data) as RawKeyEventDataFuchsia).codePoint));
-      } else if (keyEvent.data.logicalKey == LogicalKeyboardKey.enter) {
-        _controller.sink.add(lineFeed);
-      } else if (keyEvent.data is RawKeyEventDataWeb) {
-        _controller.sink.add(((keyEvent.data) as RawKeyEventDataWeb).keyLabel);
-      } else if (keyEvent.data is RawKeyEventDataLinux) {
-        _controller.sink
-            .add(((keyEvent.data) as RawKeyEventDataLinux).keyLabel);
-      } else if (keyEvent.data is RawKeyEventDataWindows) {
-        _controller.sink.add(String.fromCharCode(
-            ((keyEvent.data) as RawKeyEventDataWindows).keyCode));
-      } else if (keyEvent.data is RawKeyEventDataMacOs) {
-        _controller.sink
-            .add(((keyEvent.data) as RawKeyEventDataMacOs).characters);
-      } else if (keyEvent.data is RawKeyEventDataIos) {
-        _controller.sink
-            .add(((keyEvent.data) as RawKeyEventDataIos).characters);
-      } else {
-        _controller.sink.add(keyEvent.character);
-      }
+    bool invalidRange = keyEvent.logicalKey.keyId > 255;
+    bool notEnter = keyEvent.data.logicalKey != LogicalKeyboardKey.enter;
+
+    if (invalidRange && notEnter) {
+      return;
+    }
+
+    bool triggered = (!_useKeyDownEvent && keyEvent is RawKeyUpEvent) ||
+        (_useKeyDownEvent && keyEvent is RawKeyDownEvent);
+
+    if (!triggered) {
+      return;
+    }
+
+    if (keyEvent.data.logicalKey == LogicalKeyboardKey.enter) {
+      _controller.sink.add(lineFeed);
+    } else {
+      _controller.sink.add(keyEvent.character);
     }
   }
 
